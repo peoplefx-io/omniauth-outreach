51957b6f7362dc910fd7a26de335764f769ce715
Zachary <zpoley@gmail.com>
2022-07-28 16:52:30 +0000

    Update.

--- lib/omniauth/strategies/outreach.rb
+++ lib/omniauth/strategies/outreach.rb
@@ -31,6 +31,12 @@ module OmniAuth
         puts "raw_info: #{@raw_info}"
         @raw_info
       end
+
+      # Work-around for https://github.com/intridea/omniauth-oauth2/issues/93.
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
+
     end
   end
 end
