fd3a14c1708b26f629937cb96409f6887e2641c3
Zachary <zpoley@gmail.com>
2022-07-28 19:32:38 +0000

    Testing.

--- lib/omniauth/strategies/outreach.rb
+++ lib/omniauth/strategies/outreach.rb
@@ -4,7 +4,10 @@ module OmniAuth
   module Strategies
     class Outreach < OmniAuth::Strategies::OAuth2
       option :name, 'outreach'
-      option :client_options, site: 'https://api.outreach.io'
+      option :client_options, {
+				parse: :json,
+				site: 'https://api.outreach.io'
+			}
 
       uid do
         raw_info['meta']['user']['email']
@@ -26,10 +29,11 @@ module OmniAuth
       def raw_info
         puts "access_token: #{access_token.inspect}"
         # @raw_info ||= access_token.get('/oauth/token').parsed
-        @raw_info ||= access_token.get('/api/v2', headers: {
-					'Authorization' => "Bearer #{access_token.token}",
-					'Content-Type' => "application/vnd.api+json",
-				})
+        @raw_info ||= access_token.get('/api/v2')
+				#, headers: {
+				#	'Authorization' => "Bearer #{access_token.token}",
+				#	'Content-Type' => "application/vnd.api+json",
+				#})
 
         puts "raw_info: #{@raw_info}"
 				puts "status: #{@raw_info.status}"
