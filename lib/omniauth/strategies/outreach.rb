7449e8c2b527ef9676b5c12b4212cd4a8e4a9b21
Zachary <zpoley@gmail.com>
2022-07-29 01:48:54 +0000

    Update credentials format for parity. Remove debugging comments.

--- lib/omniauth/strategies/outreach.rb
+++ lib/omniauth/strategies/outreach.rb
@@ -17,7 +17,11 @@ module OmniAuth
 				{
 					access_token: access_token.token,
 					expires_at: access_token.expires_at,
+					expires_in: access_token.expires_in,
 					refresh_token: access_token.refresh_token,
+					scope: access_token.params["scope"],
+					created_at: access_token.params["created_at"],
+					token_type: access_token.params["token_type"]
 				}
 			end
 
@@ -36,31 +40,13 @@ module OmniAuth
       end
 
       def raw_info
-         puts "access_token: #{access_token.inspect}"
-        # @raw_info ||= access_token.get('/oauth/token').parsed
         @raw_info ||= access_token.get('/api/v2').parsed
-				#, headers: {
-				#	'Authorization' => "Bearer #{access_token.token}",
-				#	'Content-Type' => "application/vnd.api+json",
-				#})
-
-        #puts "raw_info: #{@raw_info}"
-				#puts "status: #{@raw_info.status}"
-				#puts "headers: #{@raw_info.headers}"
-				#puts "body: #{@raw_info.body}"
-
-
-				#@raw_info = @raw_info.parsed
-        #puts "raw_info.parsed: #{@raw_info}"
-
-        @raw_info
       end
 
       # Work-around for https://github.com/intridea/omniauth-oauth2/issues/93.
       def callback_url
         options[:redirect_uri] || (full_host + script_name + callback_path)
       end
-
     end
   end
 end
